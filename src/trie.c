be649e03d98495ed06d75a6d77dd97a8af22d877
franckEinstein90 <franckbinard@gmail.com>
2017-01-14 19:12:50 -0500

    modification to trie

--- src/trie.c
+++ src/trie.c
@@ -3,44 +3,166 @@
 #include "trie.h"
 
 
+
+//constructor for an array of words
+//structure
+word_list* new_word_list(int size, int max_word_size) {
+	word_list* result;
+	result = (word_list*)malloc(sizeof(word_list));
+	result->words = (char**)malloc(size * sizeof(char*));
+	result->max_word_size = 100;
+	result->max_size = size;
+	result->num_words = 0;
+	return result; 
+}
+
+void insert_word(word_list* wlist, const char* word) {
+	if (wlist->num_words < wlist->max_size) {
+		wlist->words[wlist->num_words++] = _strdup(word);
+	}
+}
+void insert_into(word_list* SuperSet, word_list* SubSet) {
+	
+}
+void print_words(word_list* wlist) {
+	int i;
+	for (i = 0; i < wlist->num_words; i++) {
+		printf("%s\n", wlist->words[i]);
+	}
+}
+
+void destroy_word_list(word_list* wlist) {
+	int i;
+	for (i = 0; i < wlist->num_words; i++) {
+		free(wlist->words[i]);
+	}
+	free(wlist->words);
+}
+
+/*************************************/
+/***************Converter Struct******/
+/*************************************/
+//converter structure holds a character, 
+//and an int representing the index
+//corresponding to that char in the 
+//trie structure
+typedef struct _converter {
+	int trie_index;
+	char character;
+	int valid; //1 if trie_index is a valid trie index
+} converter;
 int convert_to_idx(char letter) {
 	if (letter >= 'a' && letter <= 'z') return letter - 'a';
 	if (letter >= 'A' && letter <= 'Z') return letter - 'A';
 	if (letter == '\'') return 'z' - 'a' + 1;
-
+	return -1;
 }
-//returns a new tree_node
+int process_converter(converter* cv) {
+	cv->trie_index = convert_to_idx(cv->character);
+	if (cv->trie_index >= 0 && cv->trie_index <= ALPHABET_SIZE) 
+		{ cv->valid = 1; return 1; }
+	else 
+		{ cv->valid = 0; return 0; }
+}
+//constructing a new converter
+converter* new_converter(char c) {
+	converter* cv;
+	cv = (converter*)malloc(sizeof(converter));
+	cv->character = c;
+	process_converter(cv);
+	return cv;
+}
+//a new converter from a word and an index
+converter* new_converter_word_pos(const char* word, int idx) {
+	if (idx < 0 || idx > strlen(word)) {
+		return NULL;
+	}
+	return new_converter(word[idx]);
+}
+
+/******************************************/
+/********************** Tree Nodes ********/
+/******************************************/
 tree_node* init_tree(){
 	tree_node* new_tree = (tree_node*)malloc(sizeof(tree_node));
 	new_tree->character = '*';
-	for (int i = 0; i < CHILD_MAX; i++) {
+	for (int i = 0; i < ALPHABET_SIZE; i++) {
 		new_tree->children[i] = NULL;
 	}
 	new_tree->is_full_word = 0;
 	return new_tree;
 }
 tree_node* insert_at_node(tree_node* node, const char* word, size_t idx) {
-	char letter = word[idx];
-	if (letter == '\0') {
+	converter* c = new_converter_word_pos(word, idx);
+	if (c->character == '\0') {
 		node->is_full_word = 1;
 		return node;
 	}
-	int insert_pt = convert_to_idx(letter);
-	if (insert_pt >= 0 && insert_pt <= CHILD_MAX) {
-		if (node->children[insert_pt] == NULL) {
-			node->children[insert_pt] = init_tree();
-			node->children[insert_pt]->character = letter;
+	if (c->valid == 0) {
+		fprintf(stderr, "Value of errno: %d\n", errno);
+		perror("Invalid Character insert_at_node");
+		free(c);
+		return NULL;
+	}
+	if (node->children[c->trie_index] == NULL) {
+			node->children[c->trie_index] = init_tree();
+			node->children[c->trie_index]->character = c->character;
 		}
-		return insert_at_node(node->children[insert_pt], word, idx + 1);
+	tree_node* modified_node = insert_at_node(node->children[c->trie_index], word, idx + 1);
+	free(c);
+	if (modified_node == NULL) {
+		//todo error handling
 	}
-	else {
-		printf("this shouldn't happen");
-	}
+	return modified_node;
 }
+//inserts a word in the tree
+//returns 1 if succeeded
 tree_node* insert_in_tree(tree_node* tree_root, const char* word){
-	return insert_at_node(tree_root, word, 0);
+	tree_node* inserted_node;
+	inserted_node = insert_at_node(tree_root, word, 0);
+	return inserted_node;
 }
 
+
+/***************************************************/
+/*************Finding words*************************/
+/***************************************************/
+/*word_list* get_suffixes(const tree_node* node, int max_word_size) {
+//returns list of word endings starting from node argument
+	int i;
+	word_list *Asuffixes, *Osuffixes;
+	for (i = 0; i < ALPHABET_SIZE; i++) {
+		Asuffixes = new_word_list(200, max_word_size);
+		if (node->children[i] != NULL) {
+			Osuffixes = get_suffixes(node->children[i], max_word_size);
+			insert_into(Asuffixes, Osuffixes);
+			destroy_word_list(Osuffixes);
+		}
+	}
+	prefix_by(node->character, Asuffixes);
+	return Asuffixes;
+}
+
+void get_match(const trie* t, word_list* wlist, const char* pattern) {
+	int i;
+	converter* cv;
+	if (pattern[0] == '*') {
+		for (i = 0; i < ALPHABET_SIZE; i++) {
+			if (t->data->children[i]) {
+				__subfunc(t->data->children[i], wlist, pattern, 0);
+			}
+		}
+		return wlist;
+	}
+	cv = new_converter(pattern[0]);
+	if (cv->valid && t->data->children[cv->trie_index] != NULL) {
+		__subfunc(t->data->children[cv->trie_index], wlist, pattern, 0);
+	}
+}*/
+
+/*****************************************************/
+/*************Initializing a new trie*****************/
+/*****************************************************/
 trie* init_empty_trie() {
 	trie* new_trie = (trie*)malloc(sizeof(trie));
 	new_trie->num_word = 0;
@@ -70,31 +192,48 @@ trie* init_trie_from_file(const char* file_name){
     return new_trie;
 }
 
-int contains(tree_node* t, const char* word, size_t idx) {
-	char tag = word[idx];
-	if (tag == '\0' && t->is_full_word) return 1;
-	if (t->children[convert_to_idx(tag)]) {
-		return contains (t->children[convert_to_idx(tag)], word, idx + 1);
+int contains(const tree_node* t, const char* word, size_t idx) {
+	converter * cv = new_converter_word_pos(word, idx);
+	int result = 0;
+	if (cv->character == '\0' && t->is_full_word) {
+		free(cv); return 1;
 	}
-	return 0;
+	if (cv->valid == 0) {
+		//todo error handling
+		free(cv); return 0;
+	}
+	if (t->children[cv->trie_index]) result = contains (t->children[cv->trie_index], word, idx + 1);
+	free(cv);
+	return result;
 }
-
-int contains_word(trie* t, const char* word) {
+int contains_word(const trie* t, const char* word) {
 	int idx = convert_to_idx(word[0]);
-	tree_node* begin_trie = t->data->children[idx];
+	const tree_node* begin_trie = t->data->children[idx];
 	if (begin_trie != NULL) return contains(begin_trie, word, 1);
 	return 0;
 }
+void print_branch(const tree_node* trie_branch) {
 
-int find_word(trie* t, const char* word) {
-	if (contains_word(t, word)) return 1;
-	return 0;
 }
-
-trie* filter_trie(trie* source_trie, trie* dest_trie, const char* pattern, int idx) {
+void print_all(const trie* t) {
+	for (int i = 0; i < ALPHABET_SIZE; i++) {
+		if (t->data->children[i] != NULL) {
+			print_branch(t->data->children[i]);
+		}
+	}
+}
+trie* filter_trie(const trie* source_trie, trie* dest_trie, const char* pattern, int idx) {
+	converter* cv = new_converter_word_pos(pattern,idx);
+	if (cv->character == '*') {
+		free(cv);
+		return NULL;
+	}
+	if(cv->valid){
+	}
+	free(cv);
 	return NULL;
 }
-trie* list_words(trie* source, const char* pattern) {
+trie* list_words(const trie* source, const char* pattern) {
 	trie* result_trie;
 	result_trie = init_empty_trie();
 	filter_trie(source, result_trie, pattern, 0);
--- src/trie.h
+++ src/trie.h
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 
 
-#define CHILD_MAX 27
+#define ALPHABET_SIZE 27
+#define MAX_WORDS 200
+typedef struct _word_list {
+	char** words;
+	int max_word_size;
+	int max_size;
+	int num_words;
+} word_list;
+
+word_list* new_word_list(int max_size, int max_word_size);
+void destroy_word_list(word_list*);
+void insert_word(word_list* wlist, const char* new_word);
+
+
+/************************************/
+/***********Tree Nodes***************/
 typedef struct _tree_node{
     unsigned char character;    
 	int is_full_word;
-    struct _tree_node* children[CHILD_MAX];
+    struct _tree_node* children[ALPHABET_SIZE];
 } tree_node;
 
 typedef struct _trie {
@@ -24,7 +39,8 @@ void destroy_trie(trie*);
 //with additional information
 //returns 1 if the inner_tree contains the word
 //0 otherwise
-int contains_word(trie* t, const char* word);
+int contains_word(const trie* t, const char* word);
+void print_all(const trie* t);
 trie* list_words(trie* source, const char* pattern);
 
 
